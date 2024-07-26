2414f21ef657f47e60efa9973837c3d6682c940d
MatthiasWeier <matthias.weier@web.de>
2024-07-26 15:18:55 +0200

    *Refac

--- Docker Backup/Services/DockerService.cs
+++ Docker Backup/Services/DockerService.cs
@@ -1,12 +1,9 @@
-﻿using GoogleApi.Entities.Translate.Translate.Request.Enums;
-
-namespace Services;
+﻿namespace Services;
 
 internal class DockerService {
-  // todo: initialize this with StartProcessService - need to create overload 
   public static async Task<string[]> GetContainerListAsync() {
     var output = await StartProcessService.ExecuteCommand("docker","ps -a --format \"{{.ID}} {{.Image}}\"");
-    return output.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+    return output.Lines(StringSplitOptions.RemoveEmptyEntries);
   }
 
   public static async Task SaveImageAsync(string imageName, FileInfo saveContainer) {
