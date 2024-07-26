d2d01b9b959748a674b44e5cbdbc8cccf3191eb6
MatthiasWeier <matthias.weier@web.de>
2024-07-26 15:11:34 +0200

    * utilize Extension in Logger * changed DateTime.Now to DateTime.UtcNow

--- Docker Backup/Utils/Logger.cs
+++ Docker Backup/Utils/Logger.cs
@@ -2,14 +2,12 @@
 
 internal static class Logger {
   private static readonly DirectoryInfo _LOG_DIR = new(Settings.Default.BackupDestinationDirectory);
-  private static readonly FileInfo _LOG_FILE = new(Path.Combine($"{Logger._LOG_DIR.FullName}\\temp.log"));
+  private static readonly FileInfo _LOG_FILE = Logger._LOG_DIR.File("temp.log");
+
   public static void PrintAndLog(string message) {
-    var line = $"{DateTime.Now}:\t{message}";
+    var line = $"{DateTime.UtcNow}:\t{message}";
     Console.WriteLine(line);
-    if (!Logger._LOG_FILE.Exists)
-      Logger._LOG_FILE.Create();
-      
-    //todo: First time file is created this throws an exception --> fix
-    File.AppendAllText(Logger._LOG_FILE.FullName, line + Environment.NewLine);
+
+    Logger._LOG_FILE.AppendLine(line);
   }
 }
